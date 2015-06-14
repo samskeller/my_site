178fbd7dd80f1d1d9a1decd053fbad7653f71d88
Sam Keller <samskeller@gmail.com>
2015-06-13 22:51:40 -0700

    Making admin views for our models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+from blog.models import Comment
+
+class PostAdmin(admin.ModelAdmin):
+# title = models.CharField(max_length=100)
+# text = models.TextField()
+# slug = models.SlugField(max_length=60)
+    list_display = ('id', 'title', 'slug', 'date_created',)
+    list_selected_display = list_display
+    readonly_fields = ('date_created', 'date_last_modified',)
+
+class CommentAdmin(admin.ModelAdmin):
+# post = models.ForeignKey(Post, on_delete=models.PROTECT, related_name='comments')
+# text = models.TextField()
+# allowed = models.BooleanField(default=True)
+    list_display = ('id', 'post', 'allowed', 'date_created',)
+    list_display_selected = list_display
+    readonly_fields = ('date_created', 'date_last_modified',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+
--- person_details/admin.py
+++ person_details/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from person_details.models import Link
+from person_details.models import LinkClick
+
+
+class LinkAdmin(admin.ModelAdmin):
+# url = models.URLField()
+# title = models.CharField(max_length=30)
+# icon = models.ImageField(upload_to='link_icons/', blank=True, null=True)
+    list_display = ('id', 'url', 'title', 'date_created',)
+    list_selected_display = list_display
+    readonly_fields = ('date_created', 'date_last_modified',)
+
+class LinkClickAdmin(admin.ModelAdmin):
+# link = models.ForeignKey(Link, on_delete=models.PROTECT)
+    list_display = ('id', 'link', 'date_created',)
+    list_selected_display = list_display
+    readonly_fields = ('date_created', 'date_last_modified',)
+
+admin.site.register(Link, LinkAdmin)
+admin.site.register(LinkClick, LinkClickAdmin)

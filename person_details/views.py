18443843c53311936e34fd87cfcd0cb72d7fd4da
Sam Keller <samskeller@gmail.com>
2015-07-18 16:49:47 -0700

    Adding some text for an About Me page. Also starting to style the Nav links

--- person_details/views.py
+++ person_details/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+class AboutMe(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'about.html')

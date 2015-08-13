af81c5389e941cbbaaf4bb1363548225ee3c90ef
Sam Keller <samskeller@gmail.com>
2015-08-12 21:44:50 -0700

    Moving the blog posts with full text to home page. Making /blog show a more concise list of blog posts

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from blog.models import Post
 
 class BlogHome(View):
     def get(self, request, *args, **kwargs):
-        posts = Post.objects.all().order_by('-date_created')[:5]
+        posts = Post.objects.all().order_by('-date_created')[:20]
         return render(request, 'blog.html', {'posts': posts})
--- my_site/views.py
+++ my_site/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from blog.models import Post
+
 class Home(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
+        posts = Post.objects.all().order_by('-date_created')[:5]
+        return render(request, 'index.html', {'posts': posts})
 

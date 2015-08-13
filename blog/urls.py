42fc329ea22bf93515dff82b25a0a2199057c077
Sam Keller <samskeller@gmail.com>
2015-08-13 11:12:18 -0700

    Adding a Blog Post view for individual Posts. Adding links to those Posts as well

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 
 from blog.views import BlogHome
+from blog.views import BlogPost
 
 urlpatterns = [
     url(r'^$', BlogHome.as_view(), name="blog_list"),
+    url(r'posts/(?P<slug>[\w-]+)/$', BlogPost.as_view(), name='blog_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,19 @@ from blog.models import Post
 class BlogHome(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all().order_by('-date_created')[:20]
-        return render(request, 'blog.html', {'posts': posts})
+        return render(request, 'blog_list.html', {'posts': posts})
+
+class BlogPost(View):
+    def get(self, request, *args, **kwargs):
+        slug = kwargs.pop('slug', None)
+        if not id:
+            posts = Post.objects.all().order_by('-date_created')[:20]
+            return render(request, 'blog_list.html', {'posts': posts})
+
+        try:
+            post = Post.objects.get(slug=slug)
+        except Post.DoesNotExist:
+            posts = Post.objects.all().order_by('-date_created')[:20]
+            return render(request, 'blog_list.html', {'posts': posts})
+            
+        return render(request, 'blog_post.html', {'post': post})

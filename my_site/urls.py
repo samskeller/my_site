4447c6cfb47b77b023a719ee2c9b973e14cb07ce
Sam Keller <samskeller@gmail.com>
2015-08-23 18:34:59 -0700

    Better supporting static and media files so I can display icons for links. Also making machine-specific setting-loading section in settings file

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from my_site.views import Home
 from person_details.views import AboutMe
@@ -12,4 +14,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about/', include('person_details.urls', namespace="person_details")),
     url(r'^blog/', include('blog.urls', namespace="blog")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

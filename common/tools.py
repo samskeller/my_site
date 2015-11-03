b4849e6f4ba4dc11a08e6642e13c001ac8972a6f
Sam Keller <samskeller@gmail.com>
2015-11-02 22:34:28 -0800

    Fixing function to make valid URLs to work properly

--- common/tools.py
+++ common/tools.py
@@ -1,7 +1,7 @@
 def clean_url(url):
-    # Make sure the link starts with 'http://' so it's a valid external link
+    # Make sure the link starts with 'http://' or 'https://' so it's a valid external link
     try:
-        if url.index('http://') != 0:
+        if url.find('http://') != 0 and url.find('https://') != 0:
             url = 'http://' + url
     except ValueError:
         url = 'http://' + url

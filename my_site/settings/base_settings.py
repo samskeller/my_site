d5a69178e3819811cf4199ce0fa377787b4de172
Sam Keller <samskeller@gmail.com>
2016-03-23 16:39:47 +0300

    Taking out passwords and secret keys from settings files and putting them into __init__.py files which are now ignored by the repository

--- my_site/settings/base_settings.py
+++ my_site/settings/base_settings.py
@@ -60,7 +60,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sams_site',
         'USER': 'skeller',
-        'PASSWORD': 'holeysmokes',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
--- my_site/settings/local_dev.py
+++ my_site/settings/local_dev.py
@@ -3,8 +3,5 @@ import string
 
 from my_site.settings.base_settings import *
 
-
-SECRET_KEY = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
-
 STATIC_ROOT = os.path.join(PROJECT_DIR, "../public/static")
 MEDIA_ROOT = os.path.join(PROJECT_DIR, "../public/media")

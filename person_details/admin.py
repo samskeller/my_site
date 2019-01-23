29289e20a4f7ca95cecb811092abd70001ae516f
Sam Keller <samskeller@gmail.com>
2019-01-22 19:41:09 -0500

    Removing LinkType model

--- person_details/admin.py
+++ person_details/admin.py
@@ -1,20 +1,11 @@
 from django.contrib import admin
 
-from .models import LinkType
 from .models import Link
 from .models import LinkClick
 from .models import PersonDetail
 
 
-class LinkTypeAdmin(admin.ModelAdmin):
-# name = models.CharField(max_length=64)
-    list_display = ('id', 'name', 'date_created',)
-    list_selected_display = list_display
-    readonly_fields = ('date_created', 'date_last_modified',)
-
-
 class LinkAdmin(admin.ModelAdmin):
-# link_type = models.ForeignKey(LinkType, on_delete=models.PROTECT)
 # url = models.URLField()
 # title = models.CharField(max_length=30)
 # icon = models.ImageField(upload_to='link_icons/', blank=True, null=True)
@@ -36,7 +27,6 @@ class PersonDetailAdmin(admin.ModelAdmin):
     readonly_field = ('date_created', 'date_last_modified',)
 
 
-admin.site.register(LinkType, LinkTypeAdmin)
 admin.site.register(Link, LinkAdmin)
 admin.site.register(LinkClick, LinkClickAdmin)
 admin.site.register(PersonDetail, PersonDetailAdmin)
--- person_details/views.py
+++ person_details/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
 
-from .models import LinkType
 from .models import PersonDetail
 
 class AboutMe(View):
@@ -9,9 +8,3 @@ class AboutMe(View):
         my_description = PersonDetail.objects.filter(detail_type='description').first()
 
         return render(request, 'about.html', {'my_description': my_description})
-
-
-class LinkList(View):
-    def get(self, request, *args, **kwargs):
-        link_types = LinkType.objects.all()
-        return render(request, 'link_list.html', {'link_types': link_types})
